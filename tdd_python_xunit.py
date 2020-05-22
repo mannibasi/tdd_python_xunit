b89467ac4c3950c3afdec91695d0070c1b22632b
mannibasi <manni.basi@gmail.com>
2020-05-22 14:40:59 +0100

    Removed superfluous wasRun vars/flags

--- tdd_python_xunit.py
+++ tdd_python_xunit.py
@@ -24,10 +24,8 @@ class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
 
 
